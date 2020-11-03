0d0336352e4ffdbe826454d967d8ffebaffc688f
Fatih Mustafa Buçak <32177116+fatihbucak@users.noreply.github.com>
2020-11-03 11:04:09 +0300

    Update server.py

--- server.py
+++ server.py
@@ -76,5 +76,5 @@ app = create_app()
 
 if __name__ == "__main__":
     #port = app.config.get("PORT", 5000)
-    #app.run(host="0.0.0.0", port=port)
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
+    #app.run()
